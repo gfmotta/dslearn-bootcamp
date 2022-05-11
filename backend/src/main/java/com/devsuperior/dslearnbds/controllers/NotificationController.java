076f9ebd2d8387f7099edf66f58f341d230d4062
Gustavo Motta <guforatini@gmail.com>
2022-05-11 14:54:19 -0300

    Notifications page by user

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/controllers/NotificationController.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dslearnbds.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dslearnbds.dtos.NotificationDTO;
+import com.devsuperior.dslearnbds.services.NotificationService;
+
+@RestController
+@RequestMapping(value = "/notifications")
+public class NotificationController {
+
+	@Autowired
+	private NotificationService service;
+	
+	@GetMapping
+	public ResponseEntity<Page<NotificationDTO>> NotificationsPageByUser(Pageable pageable) {
+		Page<NotificationDTO> page = service.getLoggedUsersNotification(pageable);
+		return ResponseEntity.ok().body(page);
+	}
+}
